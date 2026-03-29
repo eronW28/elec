7cfe45307b69f96c00884c9c4c779f9a2d19a60e
Eron De Leon <johnerondeleon@gmail.com>
2026-03-29 16:45:25 +0800

    asdg

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,20 @@ module.exports = [
     files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module"
+      sourceType: "module",
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        console: "readonly",
+        navigator: "readonly",
+        confirm: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly"
+      }
     },
     rules: {
       "no-unused-vars": "warn",
-      "no-undef": "error"
+      "no-undef": "off"
     }
   }
 ];
